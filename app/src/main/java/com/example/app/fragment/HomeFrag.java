622e7f8b90e009783bc5ea5b7c9af09f8d478314
Manishmtc <manishmtc12@gmail.com>
2023-08-04 15:40:14 +0545

    Integrated API : failure>> added Buy and Sell Activity

--- app/src/main/java/com/example/app/fragment/HomeFrag.java
+++ app/src/main/java/com/example/app/fragment/HomeFrag.java
@@ -1,5 +1,6 @@
 package com.example.app.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.app.Buy_Sell.BuyActivity;
+import com.example.app.Buy_Sell.SellActivity;
 import com.example.app.R;
 
 
@@ -22,6 +26,29 @@ public class HomeFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView= inflater.inflate(R.layout.fragment_home, container, false);
+
+        //buy and sell button initialized
+        Button buyButton=rootView.findViewById(R.id.buyButton);
+        Button sellButton=rootView.findViewById(R.id.sellButton);
+
+        buyButton.setOnClickListener(view -> openBuyActivity());
+        sellButton.setOnClickListener(view -> openSellActivity());
+
+        return rootView;
     }
+
+    //BUY Transaction adding
+    private void openBuyActivity(){
+        Intent openBuy=new Intent(getActivity(), BuyActivity.class);
+        startActivity(openBuy);
+    }
+
+    //SELL Transaction adding
+    private void openSellActivity(){
+
+        Intent openSell=new Intent(getActivity(), SellActivity.class);
+        startActivity(openSell);
+    }
+
 }
--- app/src/main/java/com/example/app/fragment/MarketFrag.java
+++ app/src/main/java/com/example/app/fragment/MarketFrag.java
@@ -3,63 +3,106 @@ package com.example.app.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.app.marketDataApi.SecurityAdapter;
+import com.example.app.marketDataApi.SecurityData;
 import com.example.app.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MarketFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MarketFrag extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MarketFrag() {
+
+
+   /* public MarketFrag() {
         // Required empty public constructor
-    }
+    }*/
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MarketFrag.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MarketFrag newInstance(String param1, String param2) {
-        MarketFrag fragment = new MarketFrag();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public class MarketFrag extends Fragment {
+
+        private List<SecurityData> securityList;
+        private RecyclerView recyclerView;
+        private SecurityAdapter adapter;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_market, container, false);
+            recyclerView = rootView.findViewById(R.id.recyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            securityList = new ArrayList<>();
+            adapter = new SecurityAdapter(securityList);
+            recyclerView.setAdapter(adapter);
+
+             // Fetch data using Volley
+                fetchDataFromAPI();
+            return rootView;
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_market, container, false);
-    }
-}
+        private void fetchDataFromAPI() {
+            // Use Volley to fetch JSON data from the API
+            // Replace 'YOUR_API_URL' with the actual API endpoint URL
+            String url = "http://127.0.0.1:5000/securityDailyTradeStat/58";
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                    try {
+                        for (int i=0; i < response.length(); i++){
+                            JSONObject jsonObject=response.getJSONObject(0);
+                            String securityName=jsonObject.getString("securityName");
+                            String lastTradedPrice=jsonObject.getString("lastTradedPrice");
+
+                            SecurityData securityData=new SecurityData();
+                            securityData.setSecurityName(securityName);
+                            securityData.setLastTradedPrice(lastTradedPrice);
+
+                            securityList.add(securityData);
+
+                        }
+                        adapter.notifyDataSetChanged();
+
+                        Toast.makeText(getActivity(), response.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                    catch (JSONException e){
+                        e.printStackTrace();
+                    }
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+                        // Add the request to the Volley request queue
+                        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+                requestQueue.add(jsonArrayRequest);
+            }
+
+
+        }
+
+
+
+
